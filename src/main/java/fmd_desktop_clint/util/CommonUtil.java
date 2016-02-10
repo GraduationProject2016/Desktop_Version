618960e3d2462f8eb97731a25667c17c805be462
ibrahimali123 <ibrahim.ali.0403@gmail.com>
2016-02-11 00:10:53 +0200

    delete device

--- src/main/java/fmd_desktop_clint/util/CommonUtil.java
+++ src/main/java/fmd_desktop_clint/util/CommonUtil.java
@@ -1,32 +1,44 @@
 package fmd_desktop_clint.util;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CommonUtil {
 
-	private final static String filePath = System.getenv("APPDATA") + "\\Find My Device\\configfile.txt"; 
-
 	public static boolean isAddedDevice() throws IOException {
-		File addDeviceFile = new File(filePath);
+		File addDeviceFile = new File(Constants.FILE_PATH);
 		BufferedReader brTest = new BufferedReader(new FileReader(addDeviceFile));
 		String[] arr = brTest.readLine().split(" , ");
 		return arr[0].equals("1");
 	}
 
 	public static int getUserID() throws IOException {
-		File addDeviceFile = new File(filePath);
+		File addDeviceFile = new File(Constants.FILE_PATH);
 		BufferedReader brTest = new BufferedReader(new FileReader(addDeviceFile));
 		String[] arr = brTest.readLine().split(" , ");
 		return Integer.valueOf(arr[1]);
 	}
 
 	public static int getDeviceID() throws IOException {
-		File addDeviceFile = new File(filePath);
+		File addDeviceFile = new File(Constants.FILE_PATH);
 		BufferedReader brTest = new BufferedReader(new FileReader(addDeviceFile));
 		String[] arr = brTest.readLine().split(" , ");
 		return Integer.valueOf(arr[2]);
+	}
+
+	public static void DeleteDevice() throws IOException {
+		File addDeviceFile = new File(Constants.FILE_PATH);
+		BufferedReader brTest = new BufferedReader(new FileReader(addDeviceFile));
+		String[] arr = brTest.readLine().split(" , ");
+
+		try (PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(Constants.FILE_PATH, false)))) {
+			out.println(0 + " , " + arr[1] + " , " + 0);
+		} catch (IOException e) {
+		}
 	}
 }
